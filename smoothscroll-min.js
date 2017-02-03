5134c0bc62606148eb4804b0ffb014bbe3c05525
Jonathan Chute <jochute@gmail.com>
2017-02-03 13:23:38 -0400

    Update smoothscroll-min.js

--- smoothscroll-min.js
+++ smoothscroll-min.js
@@ -1,8 +1,7 @@
 /*
-  Smooth Scroll
-  Version: 1.1
-  Developer: Jonathan Chute
-  Year: 2016
+Smooth Scroll
+Version: 1.2
+Developer: Jonathan Chute
+Year: 2017
 */
-
-!function(a){a.fn.smoothScroll=function(b){var c=a(this);if(void 0!==c[0]){var e,d=a.extend({scrollTo:"",top:0},b);c.click(function(){if(""===d.scrollTo){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var b=a(this.hash);b.length?(b=b.length?b:a("[name="+this.hash.slice(1)+"]"),e=b.offset().top):e=0}}else e=d.scrollTo;return a("html,body").animate({scrollTop:e-parseInt(a("html").css("margin-top"))-d.top},500),!1})}}}(jQuery);
+!function(a){a.fn.smoothScroll=function(b){var c=a(this);if(void 0!==c[0]){var e,d=a.extend({scrollTo:"",top:0,mobile:""},b);c.click(function(){if(""===d.scrollTo){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var b=a(this.hash);b.length?(b=b.length?b:a("[name="+this.hash.slice(1)+"]"),e=b.offset().top-parseInt(a("html").css("margin-top")),e-=""!==d.mobile&&a("body").width()<780?d.mobile:d.top):e=0}}else e=d.scrollTo;return a("html,body").animate({scrollTop:e},500),!1})}}}(jQuery);
