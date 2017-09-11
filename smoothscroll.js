648bde489cc09247bd1d60ac6af1a4f0230cc7b3
Jonathan Chute <jochute@gmail.com>
2017-09-11 19:00:32 -0300

    Large Update
    A large update to the smoothScroll function including:
    
    - Rework of code
    - Renaming of "scrollTo" option to "distance"
    - Adding in "scrollExternal" option to optionally allow new pages to scroll to their location, or load there without the scroll (default)
    - Broke code down into smaller functions to reduce code duplicated code

--- smoothscroll.js
+++ smoothscroll.js
@@ -1,6 +1,6 @@
 /*
 Smooth Scroll
-Version: 1.2
+Version: 1.3
 Developer: Jonathan Chute
 Year: 2017
 */
@@ -12,38 +12,48 @@ Year: 2017
             return;
 
         var settings = $.extend( {
-            'scrollTo': '',
+            'distance': 0,
             'top': 0,
-            'mobile': ''
+            'scrollExternal': false,
         }, options);
-        
-        var scrollTo;
 
-        mainObject.click(function() {
-            if('' === settings.scrollTo) {
-                if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-                    var target = $(this.hash);
-                    if (target.length) {
-                        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-                        scrollTo = target.offset().top - parseInt($('html').css('margin-top'));
-                        if('' !== settings.mobile && $('body').width() < 780) {
-                            scrollTo -= settings.mobile;
-                        } else {
-                            scrollTo -= settings.top;
-                        }
-                    } else {
-                        scrollTo = 0;
+        var distance = settings.distance;
+
+        mainObject.click(function(e) {
+            e.preventDefault();
+            if(!settings.distance) {
+                var href = $(this).attr('href');
+                if($(this).attr('href')[0] == '#') {
+                    if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                        var target = $(this.hash);
+                        distance = GetElementScrollTop( target );
                     }
                 }
-            } else {
-                scrollTo = settings.scrollTo;
             }
-            $('html,body').animate({
-                scrollTop: scrollTo
-            }, 500);
-
-            return false;
+            AnimateScroll( distance );
         });
+
+        if(settings.scrollExternal && window.location.hash) {
+            var target = $(window.location.hash);
+            var distance = GetElementScrollTop( target );
+            AnimateScroll(distance);
+        }
+
+        function GetElementScrollTop( element ) {
+            if (element.length) {
+                return element.offset().top - parseInt($('html').css('margin-top')) - settings.top;
+            } else {
+                return 0;
+            }
+        }
+
+        function AnimateScroll( distance ) {
+            var current = $(window).scrollTop();
+            var duration = (distance > current) ? distance - current : current - distance;
+            $('html, body').animate({
+                scrollTop: distance
+            }, duration);
+        }
 
     };
 })( jQuery );
